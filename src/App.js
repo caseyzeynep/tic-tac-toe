ca0488374e765d32d1a2877d58eb50035cb49c4d
Zeynep Dinc <zeynep.dinc@udemy.com>
2024-12-08 15:52:33 +0300

    Clean up

--- src/App.js
+++ src/App.js
@@ -54,21 +54,19 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
 
     const [history, setHistory] = useState([Array(9).fill(null)]);
-    const [xIsNext, setXIsNext] = useState(true);
     const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
 
     function handlePlay(nextSquares) {
         const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
         setHistory(nextHistory);
         setCurrentMove(currentMove + 1);
-        setXIsNext(!xIsNext);
     }
 
     const currentSquares = history[currentMove];
 
     function jumpTo(nextMove) {
         setCurrentMove(nextMove);
-        setXIsNext(nextMove % 2 === 0);
     }
     const moves = history.map((squares, move) => {
         let description;
